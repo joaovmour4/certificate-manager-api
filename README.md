9d8e8926b29b28721bdc1cf576c2a06dac33de62
joaovmour4 <joaov.mour@unifesspa.edu.br>
2024-02-29 11:55:34 -0300

    Adding CORS

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 https://pt.stackoverflow.com/questions/277299/como-deixar-o-nodejs-rodando-como-servi%C3%A7o
+BASE URL: 192.168.1.102
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,29 @@
 import express, { Application } from 'express'
 import mongoose from 'mongoose'
 import certificateRoutes from './routes/certificateRoutes'
+import helmet from 'helmet'
+
+const cors = require('cors')
 const dotenv = require('dotenv').config()
 
+// Basic App configs
 const app: Application = express()
-
+app.use(cors())
+app.use(helmet())
 app.use(express.json())
+
+// Defining routes
 app.use('/', certificateRoutes)
-app.get('/', (req, res)=>{
+app.get('*', (req, res)=>{
     return res.status(200).json({message: 'Server running.'})
 })
 
+// Connecting with DB and running the App
 mongoose.set('strictQuery', false)
 const mongodb = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zdqjo6k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-
 async function main(){
     await mongoose.connect(mongodb)
 }
-
 main()
 .then(()=>{
     app.listen(3000, ()=>{
