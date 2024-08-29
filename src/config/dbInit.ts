f87220d303382276952828317a4e66c1945ef3fd
joaovmour4 <joaov.mour@unifesspa.edu.br>
2024-08-29 10:00:20 -0300

    retirando consultas de desenvolvimento

--- src/config/dbInit.ts
+++ src/config/dbInit.ts
@@ -1,6 +1,4 @@
-import { QueryTypes } from "sequelize";
 import sequelize from "./sequelize";
-import fs from 'fs'
 
 const env = process.env.ENVIRONMENT === 'DEV' // true or false
 
@@ -9,13 +7,6 @@ const dbInit = async () =>{
         alter: false,
         force: false
     })
-    // if(env){
-    //     const sql = fs.readFileSync('C:\\Users\\Cliente\\Documents\\JS\\certificate-manager-api\\src\\config\\preenchimento-tabelas.sql', 'utf-8')
-    //     const queries = sql.split(';').map(query => query.trim()).filter(query => query)
-    //     for (const query of queries) {
-    //         await sequelize.query(query, { type: QueryTypes.RAW });
-    //     }
-    // }
 }
 
 export default dbInit
