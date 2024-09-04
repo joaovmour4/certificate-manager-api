c1de1bbcce6e13372441a1f2ea459b65ef3f25f2
joaovmour4 <joaov.mour@unifesspa.edu.br>
2024-09-04 10:32:22 -0300

    att

--- src/config/dbInit.ts
+++ src/config/dbInit.ts
@@ -1,12 +1,10 @@
-import { QueryTypes } from "sequelize";
 import sequelize from "./sequelize";
-import fs from 'fs'
 
 const env = process.env.ENVIRONMENT === 'DEV' // true or false
 
 const dbInit = async () =>{
     await sequelize.sync({
-        alter: true,
+        alter: false,
         force: false
     })
 }
